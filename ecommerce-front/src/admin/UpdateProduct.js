5b60320f79241c07789d5a16577e2953951da3d2
Ankush Kumar <kanku@AnkushsMacBook-Pro.local>
2022-08-02 23:37:31 +0530

    initial commit

---
+++ ecommerce-front/src/admin/UpdateProduct.js
@@ -0,0 +1,186 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '../core/Layout';
+import { isAuthenticated } from '../auth';
+import { Link } from 'react-router-dom';
+import { createProduct, getProduct, updateProduct, getCategories } from './apiAdmin';
+
+const UpdateProduct =(props) => {
+    const [values, setValues] = useState({
+        name: '',
+        description: '',
+        price: '',
+        categories: [],
+        category: '',
+        shipping: '',
+        quantity: '',
+        photo: '',
+        loading: false,
+        error: '',
+        createdProduct: '',
+        redirectToProfile: false,
+        formData: ''
+    });
+
+    const { user, token } = isAuthenticated();
+    const {
+        name,
+        description,
+        price,
+        categories,
+        category,
+        shipping,
+        quantity,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData
+    } = values;
+
+    // load categories and set form data
+    const init = async (productId) => {
+     const a = await getProduct(productId);
+     const b = await getCategories(); 
+     setValues({...values, name: a.name, description: a.description, price: a.price, category: a.category, categories: b, shipping: a.shipping, quantity: a.quantity, photo: a.photo, formData: new FormData()});
+       
+    };
+
+    useEffect(() => {
+        const productId = props.match.params.productId;
+        init(productId);
+    }, []);
+
+    const handleChange = name => event => {
+        const value = name === 'photo' ? event.target.files[0] : event.target.value;
+        formData.set(name, value);
+        setValues({ ...values, [name]: value });
+    };
+
+    const clickSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: '', loading: true });
+
+        updateProduct(props.match.params.productId, user._id, token, formData).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
+                setValues({
+                    ...values,
+                    name: '',
+                    description: '',
+                    photo: '',
+                    price: '',
+                    quantity: '',
+                    loading: false,
+                    createdProduct: data.name
+                });
+            }
+        });
+    };
+
+    const getShip = ()=>{
+        if(values.shipping){
+            return (
+                <>
+                    <option value="1">Yes</option>
+                    <option value="0">No</option>
+                </>
+            )
+        }
+        else{
+            return (
+                <>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </>
+            )
+        }
+    }
+
+    const newPostForm = () => (
+        <form className="mb-3" onSubmit={clickSubmit}>
+            <h4>Post Photo</h4>
+            <div className="form-group">
+                <label className="btn btn-secondary">
+                    <input onChange={handleChange('photo')} type="file" name="photo" accept="image/*" />
+                </label>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={handleChange('name')} type="text" className="form-control" value={name} />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Description</label>
+                <textarea onChange={handleChange('description')} className="form-control" value={description} />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Price</label>
+                <input onChange={handleChange('price')} type="number" className="form-control" value={price} />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Category</label>
+                <select onChange={handleChange('category')} className="form-control">
+                    <option key={0} value={category._id}>{category.name}</option>
+                    {categories &&
+                        categories.map((c, i) => (
+                           (c._id != category._id)&&(<option key={i+1} value={c._id}>
+                                {c.name}
+                            </option>)
+                        ))}
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Shipping</label>
+                <select onChange={handleChange('shipping')} className="form-control">
+                    {getShip()}
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Quantity</label>
+                <input onChange={handleChange('quantity')} type="number" className="form-control" value={quantity} />
+            </div>
+
+            <button className="btn btn-outline-primary">Update Product</button>
+        </form>
+    );
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div className="alert alert-info" style={{ display: createdProduct ? '' : 'none' }}>
+            <h2>{`${createdProduct}`} is created!</h2>
+        </div>
+    );
+
+    const showLoading = () =>
+        loading && (
+            <div className="alert alert-success">
+                <h2>Loading...</h2>
+            </div>
+        );
+
+    return (
+        <Layout title="Update a product" description={`G'day ${user.name}, ready to update a product?`}>
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    {showLoading()}
+                    {showSuccess()}
+                    {showError()}
+                    {newPostForm()}
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default UpdateProduct;
